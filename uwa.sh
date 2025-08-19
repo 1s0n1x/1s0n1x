4311bdf16cacb0991bbb9396ff3aaa2c376875c6
Hyram Escalante (1s0n1x) <80212291+1s0n1x@users.noreply.github.com>
2025-08-19 10:42:35 -0400

    Create uwa.sh
    [#XXX]: Desbloqueo de acceso a red AP

---
+++ uwa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Hola, Unkn0wn"
+
+if [ "$EUID" -ne 0 ]; then
+        echo "Este script necesita privilegios de SuperUsuario"
+        sudo
+        exec sudo bash "$0" "$@"
+fi
+
+echo "Bienvenido, SuperUsuario. Espere un momento..."
+sudo sysctl -w  net.ipv4.ip_forward=1
+sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -A FORWARD -i wlo1 -o enp2s0 -j ACCEPT
+sudo iptables -A FORWARD -i enp2s0 -o wlo1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o enp2s0 -j MASQUERADE
+
+echo "Desbloqueo exitoso, Happy Hacking!"
